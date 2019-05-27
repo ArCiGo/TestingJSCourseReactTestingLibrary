db661a4b1ca91d2c006df1be660a7bb975c40b92
ArCiGo <armandocifuentes_1@hotmail.com>
2019-05-27 12:58:09 -0500

    Add of react-don.js test. Remove of previous tests

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -1,10 +1,12 @@
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {render} from 'react-testing-library'
 import {FavoriteNumber} from '../favorite-number'
 
-test('renders a number input with a label "Favorite Number"', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<FavoriteNumber />, div)
-  expect(div.querySelector('input').type).toBe('number')
-  expect(div.querySelector('label').textContent).toBe('Favorite Number')
+test('renders a number inout with a label "Favorite Number"', () => {
+  const {getByLabelText} = render(<FavoriteNumber />)
+  const input = getByLabelText('Favorite Number')
+
+  expect(input).toHaveAttribute('type', 'number')
 })
